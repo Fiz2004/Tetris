aecb0815778c63898604ed46a3e45f5b690026ce
Fiz2004 <fiz2004@Rambler.ru>
2021-08-17 10:57:43 +0700

    Закончил движение и лом блоков при обновленном спрайте жука

--- class_beetle.js
+++ class_beetle.js
@@ -61,14 +61,14 @@ export class Beetle {
 		let isCanMove = (arrayDirectionses) => {
 			for (let directions of arrayDirectionses) {
 				let result = true;
-				let Tekx = 0;
+				let TekX = 0;
 				let TekY = 0;
 				for (let direction of directions) {
-					Tekx += direction.x;
+					TekX += direction.x;
 					TekY += direction.y;
 					// Если смещение попадает за границы стакана, сказать что туда нельзя
 					if (!this.grid.isInside({
-						x: Math.floor(this.position.x / SIZE_TILES) + Tekx,
+						x: Math.floor(this.position.x / SIZE_TILES) + TekX,
 						y: Math.floor(this.position.y / SIZE_TILES) + TekY
 					})) {
 						result = false;
@@ -76,9 +76,15 @@ export class Beetle {
 					}
 					// Проверить свободен ли элемент при смещении
 					if (!this.grid.isFree({
-						x: Math.floor(this.position.x / SIZE_TILES) + Tekx,
+						x: Math.floor(this.position.x / SIZE_TILES) + TekX,
 						y: Math.floor(this.position.y / SIZE_TILES) + TekY
 					})) {
+						if (TekY != -1)
+							if (Math.random() * 100 < PROBABILITY_EAT) {
+								this.eat = 1;
+								directions.length=directions.indexOf(direction) + 1;
+								return directions;
+							}
 						result = false;
 						break;
 					}
@@ -116,7 +122,7 @@ export class Beetle {
 		DIRECTION["LEFT"] = [DIRECTION["L0"], DIRECTION["LU"], DIRECTION["LUU"]];
 		DIRECTION["RIGHT"] = [DIRECTION["R0"], DIRECTION["RU"], DIRECTION["RUU"]];
 
-		console.log(`Меняем направление движения, текущая позиция = ${JSON.stringify(this.direction)} текущая цель ${JSON.stringify(this.move)}`);
+		//console.log(`Меняем направление движения, текущая позиция = ${JSON.stringify(this.direction)} текущая цель ${JSON.stringify(this.move)}`);
 		if (this.moves.length === 0 || this.deleteRow == 1) {
 			this.deleteRow = 0;
 			let directions;
@@ -133,28 +139,13 @@ export class Beetle {
 			}
 			else {
 				if (this.lastDirection == "L")
-					directions = isCanMove([...DIRECTION["LEFTDOWN"], ...DIRECTION["LEFT"], ...DIRECTION["RIGHT"]]);
+					directions = isCanMove([...[DIRECTION["0D"]], ...DIRECTION["LEFT"], ...DIRECTION["RIGHT"]]);
 				else
-					directions = isCanMove([...DIRECTION["RIGHTDOWN"], ...DIRECTION["RIGHT"], ...DIRECTION["LEFT"]]);
+					directions = isCanMove([...[DIRECTION["0D"]], ...DIRECTION["RIGHT"], ...DIRECTION["LEFT"]]);
 			}
-
-			/*// Если двигаемся вверх
-		else if (this.getRotate(this.direction, this.move) === "0U")
-			directions = isCanMove([{ x: 0, y: -1 }, { x: 0, y: 1 }]);
-		// Если двигаемся вниз
-		else if (this.getRotate(this.direction, this.move) === "0D")
-			directions = isCanMove([{ x: 0, y: 1 }, { x: 0, y: -1 }]);
-		else if (this.move.x === 1 && this.move.y === 1)
-			directions = isCanMove([{ x: 1, y: 1 }, { x: 1, y: 0 }, { x: 0, y: -1 }]);
-		else if (this.move.x === -1 && this.move.y === 1)
-			directions = isCanMove([{ x: -1, y: 1 }, { x: -1, y: 0 }, { x: 0, y: -1 }]);
-		// Если двигаемся влево и верх на 1 клетку
-		else if (this.getRotate(this.direction, this.move) === "LU")
-			directions = isCanMove([{ x: -1, y: -1 }]);*/
 
 			this.moves = directions;
 		}
-		let tile = new Point(Math.floor(this.position.x / SIZE_TILES), Math.floor(this.position.y / SIZE_TILES));
 
 		let startMove = { ...this.move };
 		if (startMove.x === this.moves[0].x && startMove.y === this.moves[0].y) {
@@ -166,7 +157,7 @@ export class Beetle {
 		this.frames = isRotate({ ...startMove }, this.move)
 		this.direction = { ...startMove };
 
-		console.log(`Меняем направление движения, занятая позиция = ${JSON.stringify(this.direction)} Выбранная цель${JSON.stringify(this.move)}`);
+		//console.log(`Меняем направление движения, занятая позиция = ${JSON.stringify(this.direction)} Выбранная цель ${JSON.stringify(this.move)}`);
 	};
 
 	//Метод движения жука
@@ -177,8 +168,32 @@ export class Beetle {
 				this.position.x += this.move.x * SIZE_TILES / NUMBER_FRAMES_BEEATLE;
 			this.position.y += this.move.y * SIZE_TILES / NUMBER_FRAMES_BEEATLE;
 		}
+		if (this.eat === 1 && (this.direction.x === this.move.x && this.direction.y === this.move.y)
+			&& this.framesAnimation !== this.frames - 1) {
+			let offsetX = this.move.x;
+			let offsetY = this.move.y;
+			let direction;
+			if (offsetX === -1 && offsetY === 0) direction = "R";
+			if (offsetX === 1 && offsetY === 0) direction = "L";
+			if (offsetX === 0 && offsetY === 1) direction = "U";
+			if (offsetX === -1) offsetX = 0;
+			let tile = new Point(Math.floor(this.position.x / SIZE_TILES), Math.floor(this.position.y / SIZE_TILES));
+			this.grid.space[tile.y + offsetY][tile.x + offsetX].status[direction]
+				= Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_ELEMENTS))+1;
+			console.log(`Жук находится в позиции ${JSON.stringify(tile)}`);
+			console.log(`Текущий кадр ${this.framesAnimation}`);
+			console.log(`Ломаем элемент X=${tile.x + offsetX}, Y=${tile.y + offsetY}`);
+			console.log(`Значение= ${Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_ELEMENTS))+1}`);
+		}
+
 		//Определяем текущий кадр
-		if (this.framesAnimation == this.frames - 1) {
+		if (this.framesAnimation === this.frames - 1) {
+			if (this.eat === 1 && this.frames == NUMBER_FRAMES_BEEATLE) {
+				this.eat = 0;
+				let tile = new Point(Math.floor(this.position.x / SIZE_TILES), Math.floor(this.position.y / SIZE_TILES));
+				this.grid.space[tile.y][tile.x].status = { L: 0, R: 0, U: 0 };
+				this.grid.space[tile.y][tile.x].element = 0;
+			}
 			this.getTrafficBeetle();
 			this.framesAnimation = 0;
 		}
@@ -189,7 +204,7 @@ export class Beetle {
 		//console.log(`Текущая позиция = ${JSON.stringify(this.position)} на кадре ${this.framesAnimation}`);
 	};
 
-	// Провермяем есть ли доступ к верху стакана
+	// Проверяем есть ли доступ к верху стакана
 	isBreath() {
 		return true;
 	};
@@ -262,173 +277,154 @@ export class Beetle {
 		//console.log(`При выборе спрайта ${this.getRotate(this.direction, this.move)}`);
 
 		let code = "" + [this.direction.x, this.direction.y, this.move.x, this.move.y].join("");
-		switch (code){
-			// Поворот Лево->Лево
-			case "-10-10":
-				return this.framesAnimation === 0
-					? [6, 0]
-					: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 2];
-				break;
-			// Поворот Лево->0
-			case "-1000":
-				return this.framesAnimation === 0 ? [6, 0] : [7, 0];
-				break;
-			// Поворот Лево->Направо
-			case "-1010":
-				return [[6, 0], [7, 0], [0, 0], [1, 0], [2, 0]][this.framesAnimation];
-				break;
-			// Поворот Лево->Наверх
-			case "-100-1":
-				return this.framesAnimation === 0 ? [6, 0] : [5, 0];
-				break;
-			// Поворот Лево->Вниз
-			case "-1001":
-				return this.framesAnimation === 0 ? [6, 0] : [7, 0];
-				break;
+		if (this.eat === 1 && this.frames !== NUMBER_FRAMES_BEEATLE - 1
+			&& (this.direction.x === this.move.x && this.direction.y === this.move.y)) {
+			// Если жук ест
+			switch (code) {
+				// Поворот Лево->Лево
+				case "-10-10":
+					return this.framesAnimation === 0
+						? [6, 0]
+						: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 6];
+					break;
 
-			// Поворот Наверх->Налево
-			case "0-1-10":
-				return this.framesAnimation === 0 ? [4, 0] : [5, 0];
-				break;
-			// Поворот Наверх->0
-			case "0-100":
-				return [[4, 0], [3, 0], [2, 0], [1, 0], [0, 0]][this.framesAnimation];
-				break;
-			// Поворот Наверх->Направо
-			case "0-110":
-				return this.framesAnimation === 0 ? [4, 0] : [3, 0];
-				break;
-			// Поворот Наверх->Наверх
-			case "0-10-1":
-				return this.framesAnimation === 0
-				? [4, 0]
-					: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 3];
-				break;
-			// Поворот Наверх->Вниз
-			case "0-101":
-				return [[4, 0], [3, 0], [2, 0], [1, 0], [0, 0]][this.framesAnimation];
-				break;
+				// Поворот Вниз->Вниз
+				case "0101":
+					return this.framesAnimation === 0
+						? [0, 0]
+						: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 8]; break;
 
-			// Поворот 0->Налево
-			case "00-10":
-				return this.framesAnimation === 0 ? [0, 0] : [7, 0];
-				break;
-			// Поворот 0->Налево
-			case "0000": return "00";
-				return [0, 0];
-				break;
-			// Поворот 0->Направо
-			case "0010":
-				return this.framesAnimation === 0 ? [0, 0] : [1, 0];
-				break;
-			// Поворот 0->Наверх
-			case "000-1":
-				return [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]][this.framesAnimation];
-				break;
-			// Поворот 0->Вниз
-			case "0001":
-				return [0, 0];
-				break;
-			
-
-			// Поворот Вниз->Наверх
-			case "010-1":
-				return [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]][this.framesAnimation];
-				break;
-			// Поворот Вниз->0
-			case "0100":
-				return [0, 0];
-				break;
-			// Поворот Вниз->Вниз
-			case "0101":
-				return this.framesAnimation === 0
-				? [0, 0]
-				: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 4];; break;
-			// Поворот Вниз->Налево
-			case "01-10":
-				return this.framesAnimation === 0 ? [0, 0] : [7, 0];
-				break;
-			// Поворот Вниз->Направо
-			case "0110":
-				return this.framesAnimation === 0 ? [0, 0] : [1, 0];
-				break;
-
-			// Поворот Направо->Наверх
-			case "100-1":
-				return this.framesAnimation === 0 ? [3, 0] : [4, 0];
-				break;
-			// Поворот Направо->0
-			case "1000":
-				return this.framesAnimation === 0 ? [1, 0] : [0, 0];
-				break;
-			// Поворот Направо->Вниз
-			case "1001":
-				return this.framesAnimation === 0 ? [1, 0] : [0, 0];
-				break;
-			// Поворот Направо->Налево
-			case "10-10":
-				return [[2, 0], [1, 0], [0, 0], [7, 0], [6, 0]][this.framesAnimation];
-				break;
-			// Поворот Направо->Направо
-			case "1010":
-				return this.framesAnimation === 0
-				? [2, 0]
-					: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 1];
-				break;
-		}
-
-		//!Для отладки если вдруг какое-то направление забыл
-		alert(`Функция getSprite не знает такого направления ${ JSON.stringify(this.direction) } ${ JSON.stringify(this.move)}`);
-
-		/*
-				if (model.beetle.eat == 0) {
-			console.log(model.beetle.moveX + model.beetle.moveY);
-			switch (model.beetle.moveX + model.beetle.moveY) {
-				case "00":
-					[offsetX, offsetY] = model.beetle.getSprite();
-				case "L0":
-					[offsetX, offsetY] = [...[6, 0]]; break;
-				case "R0":
-					[offsetX, offsetY] = [...[2, 0]]; break;
-				default:
-					[offsetX, offsetY] = [...[9, 9]]; break;
+				// Поворот Направо->Направо
+				case "1010":
+					return this.framesAnimation === 0
+						? [2, 0]
+						: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 5];
+					break;
 			}
+
+			//!Для отладки если вдруг какое-то направление забыл
+			alert(`Функция getSprite при еде не знает такого направления ${JSON.stringify(this.direction)} ${JSON.stringify(this.move)}`);
 		} else {
-	
-		}
+			switch (code) {
+				// Поворот Лево->Лево
+				case "-10-10":
+					return this.framesAnimation === 0
+						? [6, 0]
+						: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 2];
+					break;
+				// Поворот Лево->0
+				case "-1000":
+					return this.framesAnimation === 0 ? [6, 0] : [7, 0];
+					break;
+				// Поворот Лево->Направо
+				case "-1010":
+					return [[6, 0], [7, 0], [0, 0], [1, 0], [2, 0]][this.framesAnimation];
+					break;
+				// Поворот Лево->Наверх
+				case "-100-1":
+					return this.framesAnimation === 0 ? [6, 0] : [5, 0];
+					break;
+				// Поворот Лево->Вниз
+				case "-1001":
+					return this.framesAnimation === 0 ? [6, 0] : [7, 0];
+					break;
+
+				// Поворот Наверх->Налево
+				case "0-1-10":
+					return this.framesAnimation === 0 ? [4, 0] : [5, 0];
+					break;
+				// Поворот Наверх->0
+				case "0-100":
+					return [[4, 0], [3, 0], [2, 0], [1, 0], [0, 0]][this.framesAnimation];
+					break;
+				// Поворот Наверх->Направо
+				case "0-110":
+					return this.framesAnimation === 0 ? [4, 0] : [3, 0];
+					break;
+				// Поворот Наверх->Наверх
+				case "0-10-1":
+					return this.framesAnimation === 0
+						? [4, 0]
+						: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 3];
+					break;
+				// Поворот Наверх->Вниз
+				case "0-101":
+					return [[4, 0], [3, 0], [2, 0], [1, 0], [0, 0]][this.framesAnimation];
+					break;
+
+				// Поворот 0->Налево
+				case "00-10":
+					return this.framesAnimation === 0 ? [0, 0] : [7, 0];
+					break;
+				// Поворот 0->Налево
+				case "0000": return "00";
+					return [0, 0];
+					break;
+				// Поворот 0->Направо
+				case "0010":
+					return this.framesAnimation === 0 ? [0, 0] : [1, 0];
+					break;
+				// Поворот 0->Наверх
+				case "000-1":
+					return [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]][this.framesAnimation];
+					break;
+				// Поворот 0->Вниз
+				case "0001":
+					return [0, 0];
+					break;
+
+
+				// Поворот Вниз->Наверх
+				case "010-1":
+					return [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]][this.framesAnimation];
+					break;
+				// Поворот Вниз->0
+				case "0100":
+					return [0, 0];
+					break;
+				// Поворот Вниз->Вниз
+				case "0101":
+					return this.framesAnimation === 0
+						? [0, 0]
+						: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 4];; break;
+				// Поворот Вниз->Налево
+				case "01-10":
+					return this.framesAnimation === 0 ? [0, 0] : [7, 0];
+					break;
+				// Поворот Вниз->Направо
+				case "0110":
+					return this.framesAnimation === 0 ? [0, 0] : [1, 0];
+					break;
+
+				// Поворот Направо->Наверх
+				case "100-1":
+					return this.framesAnimation === 0 ? [3, 0] : [4, 0];
+					break;
+				// Поворот Направо->0
+				case "1000":
+					return this.framesAnimation === 0 ? [1, 0] : [0, 0];
+					break;
+				// Поворот Направо->Вниз
+				case "1001":
+					return this.framesAnimation === 0 ? [1, 0] : [0, 0];
+					break;
+				// Поворот Направо->Налево
+				case "10-10":
+					return [[2, 0], [1, 0], [0, 0], [7, 0], [6, 0]][this.framesAnimation];
+					break;
+				// Поворот Направо->Направо
+				case "1010":
+					return this.framesAnimation === 0
+						? [2, 0]
+						: [Math.floor(this.framesAnimation / (NUMBER_FRAMES_BEEATLE / NUMBER_FRAMES_BEEATLE_MOVE)), 1];
+					break;
+			}
+
+			//!Для отладки если вдруг какое-то направление забыл
+			alert(`Функция getSprite не знает такого направления ${JSON.stringify(this.direction)} ${JSON.stringify(this.move)}`);
+
 		
-		if (model.beetle.eat == 0 && (model.beetle.moveY == "0" || (model.beetle.framesAnimation % 2 == 0 && model.beetle.moveY != "U" && model.beetle.moveY != "UU")))
-			[offsetX, offsetY] = [...[0, 0]];
-		// Показываем анимацию при движении влево
-		else if (model.beetle.moveX == "L" && model.beetle.moveY == "0")
-			if (model.beetle.eat == 0)
-				[offsetX, offsetY] = [...[1 * SIZE_TILES, 0]];
-			else
-				if (model.beetle.framesAnimation % 2 == 0)
-					[offsetX, offsetY] = [...[1 * SIZE_TILES, 1 * SIZE_TILES]];
-				else
-					[offsetX, offsetY] = [...[2 * SIZE_TILES, 1 * SIZE_TILES]];
-		// Показываем анимацию при движении вправо
-		else if (model.beetle.moveX == "R" && model.beetle.moveY == "0")
-			if (model.beetle.eat == 0)
-				[offsetX, offsetY] = [...[2 * SIZE_TILES, 0]];
-			else
-				if (model.beetle.framesAnimation % 2 == 0)
-					[offsetX, offsetY] = [...[1 * SIZE_TILES, 2 * SIZE_TILES]];
-				else
-					[offsetX, offsetY] = [...[2 * SIZE_TILES, 2 * SIZE_TILES]];
-		else if (model.beetle.moveX == "L" && (model.beetle.moveY == "U" || model.beetle.moveY == "UU"))
-			[offsetX, offsetY] = [...[0, 1 * SIZE_TILES]];
-		else if (model.beetle.moveX == "R" && (model.beetle.moveY == "U" || model.beetle.moveY == "UU"))
-			[offsetX, offsetY] = [...[0, 2 * SIZE_TILES]];
-		else
-			if (model.beetle.eat == 0)
-				[offsetX, offsetY] = [...[3 * SIZE_TILES, 0]];
-			else
-				if (model.beetle.framesAnimation % 2 == 0)
-					[offsetX, offsetY] = [...[1 * SIZE_TILES, 3 * SIZE_TILES]];
-				else
-					[offsetX, offsetY] = [...[2 * SIZE_TILES, 3 * SIZE_TILES]];
-	*/
+		}
 	}
+
 }
